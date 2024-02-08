3a07df33bafb641f90b29b8dfb87f0e0602b01ba
Liam Boston <lvb5483@psu.edu>
2024-02-08 14:28:34 -0500

    Created homepage

---
+++ app/src/main/java/edu/psu/sweng888/activesync/Homepage.java
@@ -0,0 +1,59 @@
+package edu.psu.sweng888.activesync;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Homepage extends AppCompatActivity {
+    // declare UI element references
+    private TextView greetingTextView;
+    private Button logWorkoutButton;
+    private Button recoveryStatusButton;
+    private Button trackProgressButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+
+        // initialize UI element references
+        greetingTextView = findViewById(R.id.greeting_textview);
+        logWorkoutButton = findViewById(R.id.log_workout_button);
+        recoveryStatusButton = findViewById(R.id.recovery_status_button);
+        trackProgressButton = findViewById(R.id.track_progress_button);
+
+        /**
+         * onClick listener for logWorkoutButton
+         */
+        logWorkoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // do something
+            }
+        });
+
+        /**
+         * onClick listener for recoveryStatusButton
+         */
+        recoveryStatusButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // do something
+            }
+        });
+
+        /**
+         * onClick listener for trackProgressButton
+         */
+        trackProgressButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // do something
+            }
+        });
+    }
+}
