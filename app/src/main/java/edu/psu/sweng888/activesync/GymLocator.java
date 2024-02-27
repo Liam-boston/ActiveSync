e3dde5bade0d5594fe7f2da73520af9d7ac5d503
James Keenan <monsterbrains@protonmail.com>
2024-02-27 14:03:25 -0500

    Created new class for implementation of google maps api gym locator. Set visibility to home page buttons to "gone".
    Currently working on RoomsDB and reading through documentation for simplifying the navBar - https://developer.android.com/guide/navigation/integrations/ui

---
+++ app/src/main/java/edu/psu/sweng888/activesync/GymLocator.java
@@ -0,0 +1,24 @@
+package edu.psu.sweng888.activesync;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.fragment.app.Fragment;
+
+public class GymLocator extends Fragment {
+
+    public GymLocator() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_gym_locator, container, false);
+    }
+
+
+}
