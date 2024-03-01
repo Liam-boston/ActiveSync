8baed4082dec3df50dfcb6f44f03a894aba43a1c
James Keenan <monsterbrains@protonmail.com>
2024-03-01 05:11:40 -0500

    - Implemented Google Maps API within the GymLocator fragment. - Working on search functionality within the map.

--- app/src/main/java/edu/psu/sweng888/activesync/GymLocator.java
+++ app/src/main/java/edu/psu/sweng888/activesync/GymLocator.java
@@ -5,20 +5,76 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
-public class GymLocator extends Fragment {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class GymLocator extends Fragment implements OnMapReadyCallback {
 
     public GymLocator() {
         // Required empty public constructor
     }
 
+    private GoogleMap gymMap;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_gym_locator, container, false);
+
+        // Initialize Gym Locator View
+        View view = inflater.inflate(R.layout.fragment_gym_locator, container, false);
+
+        //Initialize Map Fragment
+        SupportMapFragment supportMapFragment = (SupportMapFragment)
+                getChildFragmentManager().findFragmentById(R.id.map);
+
+        //Async Map
+        supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(@NonNull GoogleMap googleMap) {
+                //Map Loaded
+                googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+                    @Override
+                    public void onMapClick(@NonNull LatLng latLng) {
+                        //Map Click, initialize marker
+                        MarkerOptions markerOptions = new MarkerOptions();
+                        //Set position of marker
+                        markerOptions.position(latLng);
+                        //Title of marker
+                        markerOptions.title(latLng.latitude + " : " + latLng.longitude);
+                        //Remove markers
+                        googleMap.clear();
+                        //Zoom to marker
+                        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom (
+                                latLng, 10
+                        ));
+
+                        //Marker on map
+                        googleMap.addMarker(markerOptions);
+
+                        }
+
+
+                });
+            }
+        }); // End of Async Map
+
+        // Return Map View
+        return view;
+
+
+    } // End of onCreateView
+
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
     }
-
-
 }
--- app/src/main/java/edu/psu/sweng888/activesync/MainActivity.java
+++ app/src/main/java/edu/psu/sweng888/activesync/MainActivity.java
@@ -3,12 +3,25 @@ package edu.psu.sweng888.activesync;
 import android.os.Bundle;
 import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
-public class MainActivity extends AppCompatActivity {
+import edu.psu.sweng888.activesync.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity  {
     // declare nav-bar and fragments
     private BottomNavigationView navBar;
     private Homepage homepageFragment;
@@ -16,14 +29,23 @@ public class MainActivity extends AppCompatActivity {
     private LogWorkout logWorkoutFragment;
     private RecoveryStatus recoveryStatusFragment;
     private TrackProgress trackProgressFragment;
-    private GymLocator gymLocatorFragment;
+
+    /** Don't need this here most likely
+     * Map already integrated on GymLocator Fragment
+     * Only 5 elements allowed in bottom navigation **/
+    //private GymLocator gymLocatorFragment;
+    //private GoogleMap gymMap;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // initialize nav-bar element references
+
+
+        /** Bottom Navigation Elements and Implementation **/
+        // Initialize nav-bar element references
         navBar = findViewById(R.id.nav_bar);
         homepageFragment = new Homepage();
         calendarFragment = new Calendar();
@@ -31,13 +53,13 @@ public class MainActivity extends AppCompatActivity {
         recoveryStatusFragment = new RecoveryStatus();
         trackProgressFragment = new TrackProgress();
 
-
+        // Only 5 buttons allowed in navBar
         /*gymLocatorFragment = new GymLocator();*/
 
-        // this will display the Homepage on initial launch (instead of the nav-bar)
+        // Initialize Homepage
         getSupportFragmentManager().beginTransaction().replace(R.id.nav_bar_container, homepageFragment).commit();
 
-        // onItemSelectedListener() for nav-bar functionality
+        // Bottom Navigation Logic
         navBar.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(MenuItem item) {
@@ -58,13 +80,17 @@ public class MainActivity extends AppCompatActivity {
                 } else if (id == R.id.progress_item) {
                     getSupportFragmentManager().beginTransaction().replace(R.id.nav_bar_container, trackProgressFragment).commit();
                     return true;
-                } /*else if (id == R.id.gym_item) {
-                    getSupportFragmentManager().beginTransaction().replace(R.id.nav_bar_container, gymLocatorFragment).commit();
-                    return true;
-                }*/
+                }
+                /** Only use if adding GymLocator into bottom Navigation **/
+                //else if (id == R.id.gym_item) {
+                //    getSupportFragmentManager().beginTransaction().replace(R.id.nav_bar_container, gymLocatorFragment).commit();
+                //    return true;
+                //}
+
                 return false;
             }
         });
-    }
+
+    } //end of onCreate
 
 }
