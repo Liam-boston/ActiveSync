0f65d054e1af4ee084ef668081f02cd88f3f476a
Liam Boston <lvb5483@psu.edu>
2024-03-04 16:09:00 -0500

    added google-services id to build.gradle.kts(:app)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -44,13 +45,17 @@ dependencies {
     val room_version = "2.6.1"
     implementation("androidx.room:room-runtime:$room_version")
     annotationProcessor("androidx.room:room-compiler:$room_version")
-
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("com.google.android.gms:play-services-maps:18.2.0")
-    implementation("com.google.android.gms:play-services-maps:18.1.0")
-    implementation("com.google.android.gms:play-services-maps:18.2.0")
-    implementation ("com.google.firebase:firebase-auth:21.1.0")
+//    implementation("com.google.android.gms:play-services-maps:18.1.0")
+//    implementation("com.google.android.gms:play-services-maps:18.2.0")
+
+    // Firebase dependencies
+    // See: https://firebase.google.com/docs/auth/android/start
+    implementation(platform("com.google.firebase:firebase-bom:32.7.3"))
+    implementation ("com.google.firebase:firebase-auth")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
