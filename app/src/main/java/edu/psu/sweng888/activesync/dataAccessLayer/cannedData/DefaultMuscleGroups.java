20a43f5b8bb3127b71583d31ea1961cdcf7ae51e
Liam Boston <lvb5483@psu.edu>
2024-04-11 10:57:28 -0400

    added Shoulders, Tris, Quads, Hamstrings, and Glutes to default MuscleGroups

--- app/src/main/java/edu/psu/sweng888/activesync/dataAccessLayer/cannedData/DefaultMuscleGroups.java
+++ app/src/main/java/edu/psu/sweng888/activesync/dataAccessLayer/cannedData/DefaultMuscleGroups.java
@@ -6,24 +6,33 @@ import edu.psu.sweng888.activesync.dataAccessLayer.models.MuscleGroup;
  * A configuration-as-code list of all the default muscle groups available at app startup.
  */
 public final class DefaultMuscleGroups {
-
-    // Prevent instantiation
-    private DefaultMuscleGroups() {}
-
-    // NOTE: When defining a new default muscle group, be sure to add it to this list!
-    public static MuscleGroup[] getAllDefaultMuscleGroups(){
-        return new MuscleGroup[] {
-            Biceps,
-            Chest
-        };
-    }
-
     public static final MuscleGroup Biceps = new MuscleGroup(1l, "Biceps", 0.05);
-
     public static final MuscleGroup Chest = new MuscleGroup(2l, "Chest", 0.05);
     public static final MuscleGroup Back = new MuscleGroup(3l, "Back", 0.05);
+    public static final MuscleGroup Shoulders = new MuscleGroup(4l, "Shoulders", 0.05);
+    public static final MuscleGroup Triceps = new MuscleGroup(5l, "Triceps", 0.05);
+    public static final MuscleGroup Quadriceps = new MuscleGroup(6l, "Quadriceps", 0.05);
+    public static final MuscleGroup Hamstrings = new MuscleGroup(7l, "Hamstrings", 0.05);
+    public static final MuscleGroup Glutes = new MuscleGroup(8l, "Glutes", 0.05);
 
+    // Prevent instantiation
+    private DefaultMuscleGroups() {
+    }
 
+    // NOTE: When defining a new default muscle group, be sure to add it to this list!
+    public static MuscleGroup[] getAllDefaultMuscleGroups() {
+        return new MuscleGroup[]{
+                Biceps,
+                Chest,
+                Back,
+                Shoulders,
+                Triceps,
+                Quadriceps,
+                Hamstrings,
+                Glutes
+        };
+    }
+    
     // NOTE: This class does not have an "initialize" method like the "DefaultExerciseTypes" class
     //       because the aforementioned class's "initialize" method creates entries in the database
     //       for items from this class so long as they are linked to an exercise type.
