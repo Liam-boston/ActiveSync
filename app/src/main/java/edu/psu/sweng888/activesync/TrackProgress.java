f606cb1acf9e8c28c527ddb98ee17c09ebbe9c48
Brian Stump <bastump987@gmail.com>
2024-04-16 21:40:28 -0400

    🔨 Finish creating skeleton view for progress tracker

--- app/src/main/java/edu/psu/sweng888/activesync/TrackProgress.java
+++ app/src/main/java/edu/psu/sweng888/activesync/TrackProgress.java
@@ -3,11 +3,13 @@ package edu.psu.sweng888.activesync;
 import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -23,6 +25,8 @@ import java.util.Map;
 import edu.psu.sweng888.activesync.adapters.DebuggingTextItemAdapter;
 import edu.psu.sweng888.activesync.dataAccessLayer.cannedData.DefaultUsers;
 import edu.psu.sweng888.activesync.dataAccessLayer.db.ActiveSyncDatabase;
+import edu.psu.sweng888.activesync.dataAccessLayer.models.ExerciseType;
+import edu.psu.sweng888.activesync.dataAccessLayer.models.ExerciseTypeWithMuscleGroups;
 import edu.psu.sweng888.activesync.dataAccessLayer.models.User;
 import edu.psu.sweng888.activesync.dataAccessLayer.models.Workout;
 import edu.psu.sweng888.activesync.dataAccessLayer.viewModels.WorkoutEntryModel;
@@ -37,10 +41,32 @@ public class TrackProgress extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(
+        @NonNull LayoutInflater inflater,
+        ViewGroup container,
+        Bundle savedInstanceState
+    ) {
         // Inflate the layout for this fragment
         binding = FragmentTrackProgressBinding.inflate(inflater, container, false);
 
+        // Set up click event handlers
+        binding.trackProgressResetButton.setOnClickListener(this::onResetButtonClick);
+        binding.trackProgressSearchButton.setOnClickListener(this::onSearchButtonClick);
+
+        // Set up choices for exercise type dropdown
+        ArrayAdapter<ExerciseTypeWithMuscleGroups> exerciseTypeDropdownAdapter = new ArrayAdapter<>(
+            binding.getRoot().getContext(),
+            android.R.layout.simple_spinner_item,
+            ActiveSyncApplication.getDatabase().exerciseTypeDao().getExerciseTypesWithMuscleGroups()
+        );
+        exerciseTypeDropdownAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        binding.trackProgressExerciseTypeInput.setAdapter(exerciseTypeDropdownAdapter);
+
+
+        return binding.getRoot();
+    }
+
+    private void DEBUGGING_setupDummyChartData() {
         // TODO: DEBUGGING! Add data into the line chart to test the library's functionality.
         // See this documentation page for quick-start tips:
         //   https://weeklycoding.com/mpandroidchart-documentation/getting-started/
@@ -53,8 +79,28 @@ public class TrackProgress extends Fragment {
         LineData lineData = new LineData(dataSet);
         binding.progressTrackerLineChart.setData(lineData);
         binding.progressTrackerLineChart.invalidate();
+    }
 
-        return binding.getRoot();
+    /**
+     * Button click handler for "search" button that performs a search for the requested data and
+     * populates the graph with it.
+     * @param __ The view that triggered this event. This parameter is unused.
+     */
+    private void onSearchButtonClick(View __) {
+        this.DEBUGGING_setupDummyChartData(); // TODO: Replace w/ real impl.!
+    }
+
+    /**
+     * Button click handler for "reset" button that resets form fields.
+     * @param __ The view that triggered this event. This parameter is unused.
+     */
+    private void onResetButtonClick(View __) {
+        // Reset fields
+        binding.trackProgressStartDateInput.setText("");
+        binding.trackProgressEndDateInput.setText("");
+        // TODO: Should we really clear the chart data?
+        binding.progressTrackerLineChart.clear();
+        binding.progressTrackerLineChart.invalidate();
     }
 
 }
