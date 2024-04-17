b1e93fbaa27fb8941119634f3df60852821ccdab
Liam Boston <lvb5483@psu.edu>
2024-04-17 11:43:05 -0400

    updated progress bar with colors (red/yellow/orange/green)

--- app/src/main/java/edu/psu/sweng888/activesync/adapters/RecoveryStatusAdapter.java
+++ app/src/main/java/edu/psu/sweng888/activesync/adapters/RecoveryStatusAdapter.java
@@ -1,6 +1,8 @@
 package edu.psu.sweng888.activesync.adapters;
 
 import android.content.Context;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,8 +17,9 @@ import edu.psu.sweng888.activesync.dataAccessLayer.models.MuscleGroup;
 
 public class RecoveryStatusAdapter extends ArrayAdapter<MuscleGroup> {
     private LayoutInflater inflater;
-    private TextView muscleGroupName, recoveryStatus;
+    private TextView recoveryStatus, estimatedRecovery;
     private ProgressBar recoveryBar;
+    private Integer recStatus;
 
     public RecoveryStatusAdapter(Context context, List<MuscleGroup> muscleGroupList) {
         super(context, 0, muscleGroupList);
@@ -30,31 +33,28 @@ public class RecoveryStatusAdapter extends ArrayAdapter<MuscleGroup> {
             view = inflater.inflate(R.layout.recovery_status_list_item, parent, false);
         }
 
-        MuscleGroup muscleGroup =  getItem(position);
+        MuscleGroup muscleGroup = getItem(position);
 
-        muscleGroupName = view.findViewById(R.id.muscleGroupName);
+        // initialize UI elements
         recoveryStatus = view.findViewById(R.id.recoveryStatus);
+        estimatedRecovery = view.findViewById(R.id.estimatedRecovery);
         recoveryBar = view.findViewById(R.id.recoveryBar);
 
-        // display MuscleGroup values
-        assert muscleGroup != null;
-        muscleGroupName.setText(muscleGroup.getName());
-
         // calculate recovery status
-        Integer status = calculateRecoveryStatus(muscleGroup.getRecoveryRate());
+        assert muscleGroup != null;
+        recStatus = calculateRecoveryStatus(muscleGroup);
 
-        // display recovery status
-        String recoveryPercentage = status + "% recovered";
-        recoveryStatus.setText(recoveryPercentage);
+        // display recoveryStatus
+        String percentRecovered = muscleGroup.getName() + " \t-\t " + recStatus + "% recovered";
+        recoveryStatus.setText(percentRecovered);
 
-        /* TODO: Change progress bar color based on recovery status
-            < 10% = red
-            < 25% = orange
-            < 50% = yellow
-            < 75% = light green
-            < 100% = green
-        */
-        recoveryBar.setProgress(status);
+        // display estimatedRecovery
+        String estimate = "~" + calculateRecoveryTime(muscleGroup) + " day(s) until full recovery";
+        estimatedRecovery.setText(estimate);
+
+        // display recoveryBar
+        recoveryBar.setProgress(recStatus);
+        adjustProgressBarColors(recStatus);
 
         return view;
     }
@@ -68,11 +68,58 @@ public class RecoveryStatusAdapter extends ArrayAdapter<MuscleGroup> {
      * the recovery status of that muscle group would be 22%. The muscle group recovers 6% each day and
      * after 2 days of rest, the muscle group would be 12% more recovered than after the last workout.
      *
-     * @param rate
-     * @return Integer recoveryStatus
+     * @param muscleGroup
+     * @return Integer recoveryStatus (whole percentage)
      */
-    private Integer calculateRecoveryStatus(double rate) {
+    private Integer calculateRecoveryStatus(MuscleGroup muscleGroup) {
         // TODO: implement calculateRecoveryStatus()
-        return 50;
+        return (int) (Math.random() * 100) + 1;
+    }
+
+    /**
+     * With a muscle groups recovery rate (the rate at which it recovers from fatigue when not worked)
+     * and the time elapsed since the muscle group was last worked, this method returns the estimated
+     * number of days until the muscle group is fully recovered
+     *
+     * Example: If a muscle group is 90% fatigued after a workout 2 days ago with a recovery rate of 0.06,
+     * the estimated recovery time would be ~13 days. The muscle group recovers 6% each day and after 2 days
+     * of rest, the muscle group would be 22% recovered. With a recovery rate of 6%, it would take an additional
+     * 13 days until that muscle group was fully recovered.
+     *
+     * @param muscleGroup
+     * @return Integer recoveryTime (in days)
+     */
+    private Integer calculateRecoveryTime(MuscleGroup muscleGroup) {
+        // TODO: implement calculateRecoveryTime()
+        return 1;
+    }
+
+    /**
+     * Adjust the color of each progress bar based on the calculated recovery status for that muscle group:
+     * <10 = red
+     * <30 = orange
+     * <60 = yellow
+     * <75 = light green
+     * <100 = green
+     *
+     * @param recoveryStatus
+     */
+    private void adjustProgressBarColors(Integer recoveryStatus) {
+        if (recoveryStatus <= 10) {
+            // Red
+            recoveryBar.setProgressTintList(ColorStateList.valueOf(Color.rgb(204, 0, 0)));
+        } else if (recoveryStatus <= 30) {
+            // Orange
+            recoveryBar.setProgressTintList(ColorStateList.valueOf(Color.rgb(204, 102, 0)));
+        } else if (recoveryStatus <= 60) {
+            // Yellow
+            recoveryBar.setProgressTintList(ColorStateList.valueOf(Color.rgb(204, 204, 0)));
+        } else if (recoveryStatus <= 75) {
+            // Light green
+            recoveryBar.setProgressTintList(ColorStateList.valueOf(Color.rgb(102, 204, 0)));
+        } else if (recoveryStatus <= 100) {
+            // Green
+            recoveryBar.setProgressTintList(ColorStateList.valueOf(Color.rgb(0, 204, 0)));
+        }
     }
 }
