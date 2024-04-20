46d5797bf2584193afd344c1f2f9a0028d9f7b43
James Keenan <monsterbrains@protonmail.com>
2024-04-20 18:07:44 -0400

    pushing where left off on calendar

--- app/src/main/java/edu/psu/sweng888/activesync/GymLocator.java
+++ app/src/main/java/edu/psu/sweng888/activesync/GymLocator.java
@@ -1,14 +1,13 @@
 package edu.psu.sweng888.activesync;
 
-import static edu.psu.sweng888.activesync.R.id.mapSearch;
-
-import android.app.Activity;
+import android.content.Context;
 import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.widget.SearchView;
@@ -16,42 +15,53 @@ import androidx.fragment.app.Fragment;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
 import java.util.List;
 
 public class GymLocator extends Fragment implements OnMapReadyCallback {
+
+    private GoogleMap gymMap;
+    private SearchView mapSearching;
+    SupportMapFragment mapFragment;
 
     public GymLocator() {
         // Required empty public constructor
     }
 
-    private GoogleMap gymMap;
-    //Search View
-
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+
         // Initialize Gym Locator View
         View view = inflater.inflate(R.layout.fragment_gym_locator, container, false);
+
 
         //Initialize Map Fragment
         SupportMapFragment supportMapFragment = (SupportMapFragment)
                 getChildFragmentManager().findFragmentById(R.id.map);
 
+        SearchView mapSearching = (SearchView) view.findViewById(R.id.mapSearching);
 
+
+        //Initialize array of place types
+        String[] placeTypes = {"gym", "workout", "weight lifting"};
+        //Initialize array of place names
+        String[] placeNames = {"Gym", "Workout", "Weight Lifting"};
 
         //Async Map
         supportMapFragment.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(@NonNull GoogleMap googleMap) {
-                //Map Loaded
+
+
+                //Map Markers
                 googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
                     @Override
                     public void onMapClick(@NonNull LatLng latLng) {
@@ -70,34 +80,54 @@ public class GymLocator extends Fragment implements OnMapReadyCallback {
 
                         //Marker on map
                         googleMap.addMarker(markerOptions);
-
                         }
-
-
                 });
-
-
-
 
 
             }
         }); // End of Async Map
 
+
+
         // Return Map View
         return view;
 
-
-
-
-
     } // End of onCreateView
-
-
-
-
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
+
+        mapSearching.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+
+                String location = mapSearching.getQuery().toString();
+                List<Address> addressList = null;
+
+                if (location != null){
+                    Geocoder geocoder = new Geocoder(GymLocator.this.getActivity());
+
+                    try {
+                        addressList = geocoder.getFromLocationName(location, 1);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+
+                    Address address = addressList.get(0);
+                    LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+                    gymMap.addMarker(new MarkerOptions().position(latLng).title(location));
+                    gymMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,10));
+                }
+
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
+
 
     }
 }
--- app/src/main/java/edu/psu/sweng888/activesync/WorkoutCalendar.java
+++ app/src/main/java/edu/psu/sweng888/activesync/WorkoutCalendar.java
@@ -1,5 +1,6 @@
 package edu.psu.sweng888.activesync;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,15 +8,71 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import edu.psu.sweng888.activesync.adapters.RecoveryStatusAdapter;
+import edu.psu.sweng888.activesync.adapters.WorkoutSetAdapter;
+import edu.psu.sweng888.activesync.dataAccessLayer.cannedData.DefaultMuscleGroups;
+import edu.psu.sweng888.activesync.dataAccessLayer.db.ActiveSyncDatabase;
+import edu.psu.sweng888.activesync.dataAccessLayer.models.ExerciseType;
+import edu.psu.sweng888.activesync.dataAccessLayer.models.MuscleGroup;
+import edu.psu.sweng888.activesync.dataAccessLayer.models.User;
+import edu.psu.sweng888.activesync.dataAccessLayer.models.Workout;
+import edu.psu.sweng888.activesync.dataAccessLayer.models.WorkoutSet;
+import edu.psu.sweng888.activesync.dataAccessLayer.viewModels.WorkoutEntryModel;
 
 public class WorkoutCalendar extends Fragment {
+
+    private ListView listView;
+    private WorkoutSetAdapter adapter;
+    private List<Workout> workoutsPerformed;
+    List<Workout> workout = new ArrayList<Workout>();
+
+    private String Workout;
+    // Required empty public constructor
+
+
     public WorkoutCalendar() {
-        // Required empty public constructor
     }
+    @SuppressLint("MissingInflatedId")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calendar, container, false);
+        View view = inflater.inflate(R.layout.fragment_calendar, container, false);
+
+        // Configure the ListView
+        listView = view.findViewById(R.id.calendar_list);
+
+        // get the active user
+        User activeUser = ActiveSyncApplication.getActiveUser();
+
+        // pull workouts from the database
+        ActiveSyncDatabase db = ActiveSyncApplication.getDatabase();
+
+        List<WorkoutEntryModel> workouts;
+        try {
+            workouts = WorkoutEntryModel.allFromDatabaseByUser(db, activeUser);
+            List<Date> workoutDates = workouts.stream().map(x -> x.workout.date).collect(Collectors.toList());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+
+
+
+        //List<WorkoutSet> exerciseTypes = new ArrayList<>(Arrays.asList(ExerciseType()));
+
+
+        return view;
     }
 }
